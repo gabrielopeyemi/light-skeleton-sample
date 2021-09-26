21d270bd7a18cbaf56a155c0828cdc0a48b302f2
gabrielopeyemi <famosipe2010@gmail.com>
2021-09-26 11:50:59 +0100

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { TextSkeleton } from 'light-skeleton';
+const Styles = {
+  container: {
+    backgroundColor: '#4a5bdc',
+    color: '#fff',
+    padding: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 5,
+    marginBottom: 5,
+    marginTop: 5,
+  },
+  header: {
+    marginBottom: 5,
+    marginTop: 5,
+  },
+  body: {
+    marginBottom: 7,
+  },
+}
+const App = (): any => {
+  // types
+  interface DataArgs{
+    firstName: string;
+    lastName: string;
+    location: string
+  }
+  // state
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [MainData] = useState<DataArgs[]>([
+    {
+      firstName: 'Kenneth',
+      lastName: 'Erikson',
+      location: 'San Diego, CA',
+    },
+    {
+      firstName: 'Bolanle',
+      lastName: 'Erikson',
+      location: 'Akure, Nigeria',
+    },
+    {
+      firstName: 'Adewale',
+      lastName: 'Erikson',
+      location: 'Lagos, Nigeria',
+    },
+    {
+      firstName: 'Mary',
+      lastName: 'Erikson',
+      location: 'Accra Ghana',
+    },
+  ])
+  let loaderHeight = '80px';
+  let loaderWidth = `calc(100% - 20px)`
 
-function App() {
+  // useEffect (what happened when load)
+  useEffect(() => {
+    setTimeout(()=> setIsLoading(false), 2000)
+  }, [])
+  //function 
+  const TrueData = () => {
+    const { container, header, body } = Styles;
+    return (
+      <React.Fragment>
+        {MainData.map(({firstName, lastName, location}: DataArgs, index: number) => (
+          <div style={container} key={index}>
+            <h3 style={header}>{firstName} {lastName}</h3>
+            <h5 style={body}>{location}</h5>
+          </div>
+        ))}
+      </React.Fragment>
+    )
+  }
+  const Skeleton = () => {
+    return (
+      <React.Fragment>
+        {MainData.map(({firstName, lastName, location}: DataArgs, index: number) => (
+          <TextSkeleton height={loaderHeight} width={loaderWidth} style={{
+            marginLeft: 10,
+            marginRight: 10,
+          }}
+          key={index}
+          />
+        ))}
+      </React.Fragment>
+      
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <h1 style={{textAlign: 'center'}}>The Erikson</h1>
+      {isLoading ? <Skeleton /> : <TrueData /> }
+    </React.Fragment>
+  )
 }
 
 export default App;
